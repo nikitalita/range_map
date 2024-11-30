0320fd5d97fff54bd72b2dd39c2be4d8b88e50cb
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-11-30 15:38:56 -0500

    fix incorrect return type on _lbound, _rbound

--- range_map/range_map.hpp
+++ range_map/range_map.hpp
@@ -723,7 +723,7 @@ namespace beneficii {
             
             // see lower_bound for details
             bool _lbound(const key_type& _val, _nodeptr* _nod) const {
-                if(*_root == nullptr) return nullptr;
+                if(*_root == nullptr) return false;
                 _nodeptr _n = *_root;
                 _nodeptr _last = _n;
                 bool _ret = false;
@@ -764,7 +764,7 @@ namespace beneficii {
             }
             
             bool _ubound(const key_type& _val, _nodeptr* _nod) const {
-                if(*_root == nullptr) return nullptr;
+                if(*_root == nullptr) return false;
                 _nodeptr _n = *_root;
                 _nodeptr _last = _n;
                 bool _ret = false;
